2ffcc09e88df660d73055ea5d4499ba677463e4a
Your Name <you@example.com>
2023-08-14 17:40:48 +0200

    nat-gateway added, subnets reduction

--- website-ecs/backend.tf
+++ website-ecs/backend.tf
@@ -1,8 +1,8 @@
 # store the terraform state file in s3
 terraform {
   backend "s3" {
-    bucket    = "project-bucket-terraform8112023"
-    key       = "terraform.tfstate"
-    region    = "eu-central-1"
+    bucket = "project-bucket-terraform8112023"
+    key    = "terraform.tfstate"
+    region = "eu-central-1"
   }
 }
